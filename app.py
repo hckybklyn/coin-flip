17933e25bc0d1e4c2b12b9ae02f44fe9bd1b6cda
Artur Rusov <artur.rusov@datarobot.com>
2020-04-28 00:59:13 -0400

    display images

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from random import random
 from flask import Flask, render_template
 
+PROJECT_DIR = os.path.dirname(__file__)
+IMAGES_FOLDER = os.path.join(PROJECT_DIR, "static", "images")
 
 app = Flask(__name__, template_folder="templates")
+app.config["IMAGES"] = IMAGES_FOLDER
 
 
 @app.route("/")
@@ -10,15 +14,23 @@ def hello():
     return render_template('home.html')
 
 
+def images_file_name(name: str):
+    return os.path.join(app.config["IMAGES"], name)
+
+
 @app.route("/flip")
 def coin_flip():
     num = random()
 
     if num >= 0.5:
-        return "HEADS!"
+        image = images_file_name("heads.jpg")
+        return render_template("page_with_image.html", image=image, alt_text="HEADS!")
+        # return "HEADS!"
 
     if num < 0.5:
-        return "TAILS!"
+        image = images_file_name("tails.jpg")
+        return render_template("page_with_image.html", image=image, alt_text="TAILS!")
+        # return "TAILS!"
 
 
 if __name__ == "__main__":
